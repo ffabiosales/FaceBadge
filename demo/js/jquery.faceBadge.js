ec19dfc2f35f72510ee034cf3da463fd67ca3603
ffabiosales <ffabiosales@gmail.com>
2014-02-19 15:39:24 -0300

    plugin revisited

--- demo/js/jquery.faceBadge.js
+++ demo/js/jquery.faceBadge.js
@@ -13,7 +13,7 @@
 
         $.fn.faceBadge.defaults = {
             pageId: null,
-            loaderText: "Loading tweets...",
+            loaderText: "Loading badge...",
             width: 500,
             coverHeight: 137,
             textColor: "#999",
@@ -40,11 +40,11 @@
             //c.html('teste');
 
             // request (o.numTweets + 20) to avoid not having enough tweets if includeRetweets = false and/or excludeReplies = true
-            window.jsonTwitterFeed = "http://graph.facebook.com/" + o.pageId;
+            window.jsonPageGraph = "http://graph.facebook.com/" + o.pageId;
 
 
             //code to fetch fan page details
-            jQuery.getJSON(jsonTwitterFeed, {
+            jQuery.getJSON(jsonPageGraph, {
                 format: "json"
             })
                     .done(function(data) {
@@ -58,8 +58,8 @@
                         var coverBad = data.cover.source;
                         var cover = coverBad.replace('s720x720', 's480x480');
                         var desc = '';
-                        if(o.linkToPage) {
-                            var link = '<a target="_blank" href="'+ data['link'] +'">';
+                        if (o.linkToPage) {
+                            var link = '<a target="_blank" href="' + data['link'] + '">';
                             var linkEnd = '</a>';
                         }
                         else {
@@ -72,10 +72,10 @@
                         }
                         else {
                             fb_desc = 'No description';
-                            
+
                             if (data['about'] !== undefined) {
-                                fb_about = data['about'].toString().substr(0, 70) + ' ...';    
-                                
+                                fb_about = data['about'].toString().substr(0, 70) + ' ...';
+
                                 desc = fb_about;
                             }
                             else {
@@ -88,16 +88,16 @@
                         c.html('<div class="faceBadge-cover"></div>');
                         var fbcover = $(".faceBadge-cover");
                         fbcover.css('height', o.coverHeight);
-                        
-                        if(o.showDesc) {
-                        fbcover.append('<div class="faceBadge-desc">'+ desc +'</div>');
-                    }
-                        fbcover.append('<img style="margin-left: -110px" src="' + cover + '" /><div style="height: '+ o.coverHeight +'px ; width:100%" class="cover-shadow"></div>');
+
+                        if (o.showDesc) {
+                            fbcover.append('<div class="faceBadge-desc">' + desc + '</div>');
+                        }
+                        fbcover.append('<img style="margin-left: -110px" src="' + cover + '" /><div style="height: ' + o.coverHeight + 'px ; width:100%" class="cover-shadow"></div>');
                         //c.append('<div style="height: 100%; width:100%" class="cover-shadow"></div>');
                         c.append('<div id="faceBadge" class="faceBadge-page-data"></div>');
-                        
-                        var fbdata = $('.faceBadge-page-data');                        
-                        fbdata.append('<div class="FaceBadge-image">'+ link +'<img src="http://res.cloudinary.com/demo/image/facebook/w_100,h_100,c_fill/' + o.pageId + '.jpg" width="100" height="100"/>'+linkEnd+'</div>');
+
+                        var fbdata = $('.faceBadge-page-data');
+                        fbdata.append('<div class="FaceBadge-image">' + link + '<img src="http://res.cloudinary.com/demo/image/facebook/w_100,h_100,c_fill/' + o.pageId + '.jpg" width="100" height="100"/>' + linkEnd + '</div>');
                         fbdata.append('<div class="faceBadge-content"><div class="faceBadge-name">' + fb_name + '</div></div>');
                         fbdata.append('<div class="faceBadge-like-count"></div>');
                         $(".faceBadge-like-count").append('<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(('http://www.facebook.com/' + o.pageId)) + '&amp;width=100px&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35&amp;appId=442899805810904" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:50px; height:35px; float:left" allowTransparency="true" id="face"></iframe>');
@@ -106,26 +106,28 @@
                         c.fadeIn();
                     });
 
-            function add_commas(number) {
-                if (number.length > 3) {
-                    var mod = number.length % 3;
-                    var signal = '.';
-                    var output = (mod > 0 ? (number.substring(0, mod)) : '');
-                    for (i = 0; i < Math.floor(number.length / 3); i++) {
-                        if ((mod === 0) && (i === 0)) {
-                            output += number.substring(mod + 3 * i, mod + 3 * i + 3);
-                        } else {
-                            if(output > 1) {
-                               signal = ','; 
-                            }
-                            output += signal + number.substring(mod + 3 * i, mod + 3 * i + 3);
-                        }
-                    }
-                    return (output);
-                } else {
-                    return number;
-                }
-            }
         });
+        
+        //customize like number
+        function add_commas(number) {
+            if (number.length > 3) {
+                var mod = number.length % 3;
+                var signal = '.';
+                var output = (mod > 0 ? (number.substring(0, mod)) : '');
+                for (i = 0; i < Math.floor(number.length / 3); i++) {
+                    if ((mod === 0) && (i === 0)) {
+                        output += number.substring(mod + 3 * i, mod + 3 * i + 3);
+                    } else {
+                        if (output > 1) {
+                            signal = ',';
+                        }
+                        output += signal + number.substring(mod + 3 * i, mod + 3 * i + 3);
+                    }
+                }
+                return (output);
+            } else {
+                return number;
+            }
+        }
     };
 })(jQuery);
